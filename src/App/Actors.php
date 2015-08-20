1dbc0c52b832e11a1ec99279c9c3923040018e3f
Boyer <julien@meetserious.com>
2015-08-20 17:39:12 +0200

    first push

---
+++ src/App/Actors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+
+class Actors
+{
+
+    public function __construct(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function retrieve($limit= 10, $order = "title"){
+
+    }
+
+    public function delete($id){
+
+    }
+
+    public function update($id, array $datas){
+
+    }
+
+
+}
